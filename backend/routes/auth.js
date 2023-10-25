c3d137a2476edd5152d9e9073b696fd400ac4ba8
Nikhil Kumar <nikhilsk369@gmail.com>
2023-10-25 20:46:21 +0530

    adding signup component to iNotebook

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,16 +15,17 @@ router.post('/createuser', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 chars').isLength({ min: 5 })
 ] , async (req,res) =>{
+    let success = false;
     // If there are errorss, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({success, errors: errors.array() });
     }
     // Check whether this email exits already
     try {
         let user = await User.findOne({email: req.body.email})
         if(user){
-            return res.status(400).json({error: "Sorry this email exists already."})
+            return res.status(400).json({success, error: "Sorry this email exists already."})
         }
         // Password Gen salt
         const salt = await bcrypt.genSalt(10);
@@ -42,7 +43,9 @@ router.post('/createuser', [
             }
         }
         const authtoken = jwt.sign(data, JWT_SECRET);
-        res.json({authtoken});
+
+        success= true
+        res.json({success, authtoken});
         // res.json({user})
     } 
     // catch error
