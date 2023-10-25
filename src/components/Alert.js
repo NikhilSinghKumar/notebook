6fc9af92d735135050113ee8e422fcc48fb26dd2
Nikhil Kumar <nikhilsk369@gmail.com>
2023-10-25 22:02:15 +0530

    adding Alert to login, signup & Notes components

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-function Alert(props) {
+export default function Alert(props) {
+    const capitalize = (word) => {
+      if(word==="danger"){
+        word="Error";
+      }
+        const lower = word.toLowerCase();
+        return lower.charAt(0).toUpperCase()+lower.slice(1);
+    }
   return (
-    <div>
-        <div className="alert alert-primary" role="alert">
-            {props.message}
-        </div>
+    props.alert && <div className={`alert alert-${props.alert.typ} alert-dismissible fade show`} role="alert">
+        <strong>{capitalize(props.alert.typ)}</strong> : {props.alert.msg}
     </div>
   )
 }
-
-export default Alert
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -3,7 +3,7 @@ import noteContext from "../context/notes/noteContext";
 import Noteitem from "./Noteitem";
 import AddNote from "./AddNote";
 
-const Notes = () => {
+const Notes = (props) => {
   const context = useContext(noteContext);
   const { notes, getNotes, editNote } = context;
 
@@ -29,6 +29,7 @@ const Notes = () => {
   const handleClick = (e) => {
     editNote(note.id, note.etitle, note.edescription, note.etag)
     refClose.current.click();
+    props.showAlert("Updated successfully", "success")
   };
   const onChange = (e) => {
     setNote({ ...note, [e.target.name]: e.target.value });
@@ -36,7 +37,7 @@ const Notes = () => {
 
   return (
     <>
-      <AddNote />
+      <AddNote showAlert= {props.showAlert} />
 
       <button
       ref={ref}
@@ -134,7 +135,7 @@ const Notes = () => {
         </div>
         {notes.map((note) => {
           return (
-            <Noteitem key={note._id} updateNote={updateNote} note={note} />
+            <Noteitem key={note._id} updateNote={updateNote} showAlert={props.showAlert} note={note} />
           );
         })}
       </div>
